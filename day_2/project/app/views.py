2acee92e4c3af49844df234494fb0950cd972db8
Asile Ayuba <asileayuba@gmail.com>
2025-01-05 19:28:04 +0100

    Day 5:DJANGO AUTHENTICATION SYSTEM FOR SIGNUP PAGE (BACKEND PART)

--- day_2/project/app/views.py
+++ day_2/project/app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
@@ -14,4 +14,29 @@ def handlelogin(request):
     return render(request, 'login.html')
 
 def handlesignup(request):
+    if request.method=="POST":
+        uname = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("pass1")
+        confirmpassword = request.POST.get("pass2")
+        if password != confirmpassword:
+            return HttpResponse("Oops! Password Incorrect.")
+
+        
+        try:
+            if User.objects.get(username=uname):
+                return HttpResponse("ALREADY USED")
+        except:
+            pass
+        
+        try:
+            if User.objects.get(email=email):
+                return HttpResponse("EMAIL USED ALREADY")
+        except:
+            pass
+            
+        myuser = User.objects.create_user(uname, email, password)
+        myuser.save()
+        return HTTPResponse("Signup Successfully!")
+    
     return render(request, 'signup.html')
