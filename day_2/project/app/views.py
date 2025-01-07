7c5cf19cc0adb71f2fb5214b53edd424fe05298c
Asile Ayuba <asileayuba@gmail.com>
2025-01-07 19:55:56 +0100

    DAY 7:Django Login, Logout Authentication System in my website

--- day_2/project/app/views.py
+++ day_2/project/app/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
+
 
 def handlelogin(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        uname = request.POST.get("username")
+        pass1 = request.POST.get("pass1")
+        myuser = authenticate(username=uname, password=pass1)
+        if myuser is not None:
+            login(myuser)
+            messages.success(request, "Login Successfully!")
+            return redirect("/")
+        else:
+            messages.error(request, "Invalid Credentials")
+            return redirect("/login")
+
+    return render(request, "login.html")
+
 
 def handlesignup(request):
-    if request.method=="POST":
+    if request.method == "POST":
         uname = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("pass1")
@@ -30,17 +48,17 @@ def handlesignup(request):
             return redirect("/signup")
         except:
             pass
-        
+
         try:
             if User.objects.get(email=email):
                 messages.info(request, "Email Is Taken")
             return redirect("/signup")
         except:
             pass
-            
+
         myuser = User.objects.create_user(uname, email, password)
         myuser.save()
         messages.success(request, "Signup Successfully! Please Login")
         return redirect("/login")
-    
-    return render(request, 'signup.html')
+
+    return render(request, "signup.html")
