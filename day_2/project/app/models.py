6439d810278639518602c68b7cf01dfa0261f4e2
Asile Ayuba <asileayuba@gmail.com>
2025-01-11 19:56:39 +0100

    DAY 11: Django Models, Creating Contact Page & Storing Data In Aministration.

--- day_2/project/app/models.py
+++ day_2/project/app/models.py
@@ -6,4 +6,5 @@ class Contact(models.Model):
     email = models.EmailField()
     phoneNumber = models.CharField(max_length=12)
     description = models.TextField()
-    
+    def __str__(self):
+        return f"Message from {self.name}"
