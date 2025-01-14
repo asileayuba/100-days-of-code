95472439880f70f9ee4716ed292bc1dd01e43502
Asile Ayuba <asileayuba@gmail.com>
2025-01-14 18:07:41 +0100

    Day 14: Creating Django Blog Page and Displaying Blog Content Dynamically

--- day_2/project/app/models.py
+++ day_2/project/app/models.py
@@ -15,4 +15,5 @@ class Blogs(models.Model):
     authname=models.CharField(max_length=50)
     img=models.ImageField(upload_to='pics', blank=True, null=True)
     timeStamp=models.DateTimeField(auto_now_add=True)
-    
+    def __str__(self):
+        return f"Uploaded by {self.authname}"
