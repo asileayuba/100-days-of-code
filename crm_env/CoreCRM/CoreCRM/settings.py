7bbdea24470877f809b3b03cd5f6103b8f5918e5
Asile Ayuba <asileayuba@gmail.com>
2025-01-19 22:36:58 +0100

    Day 19: Building CRM (Customer Relationship Management) App with Django, Python, and MySQL

--- crm_env/CoreCRM/CoreCRM/settings.py
+++ crm_env/CoreCRM/CoreCRM/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "CoreCRM",
         "USER": "root",
-        "PASSWORD": "Asile2024$",
+        "PASSWORD": "your-password",
         "HOST": "localhost",
         "PORT": "3306",
     }
--- crm_env/CoreCRM/mydb.py
+++ crm_env/CoreCRM/mydb.py
@@ -4,7 +4,7 @@ import mysql.connector
 dataBase = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    passwd = 'Asile2024$',
+    passwd = 'your-password',
     
 )
 
