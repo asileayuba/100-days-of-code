675e00b0bcd69845b29e3cd0e4b2b4c925edc70a
Asile Ayuba <asileayuba@gmail.com>
2025-01-24 20:22:23 +0100

    Day 24: Building CRM (Customer Relationship Management) App with Django, Python, and MySQL

--- crm_env/CoreCRM/crm/views.py
+++ crm_env/CoreCRM/crm/views.py
@@ -83,3 +83,14 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must be logged in to view that page.")
         return redirect('home')
+    
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Records deleted successfully.")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to delete record.")
+        return redirect('home')
