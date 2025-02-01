e2303359c60bad3e357f1507d819866e5fd42dc2
Asile Ayuba <asileayuba@gmail.com>
2025-02-01 21:20:21 +0100

    Day 32: Building an Analytics Dashboard

--- analytics/core/api/views.py
+++ analytics/core/api/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from random import randint
-
+from api.models import TotalViewsModel
 
 def total_views(request):
-    return JsonResponse({
-        "labels": ["Aug", "Sept", "Oct", "Nov", "Dec", "Jan"],
-        "data": [randint(1000*i, 1000*(i+10)) for i in range(6)],
-    })
+    queryset = TotalViewsModel.objects.all()
+    dct = {
+        "labels": [],
+        "data": [],
+    }
+    print(quer)
+    for item in queryset:
+        dct["labels"].append(item.label)
+        dct["data"].append(item.views)
+    print(dct)
+
+
+
+    # return JsonResponse({
+    #     "labels": ["Aug", "Sept", "Oct", "Nov", "Dec", "Jan"],
+    #     "data": [randint(1000*i, 1000*(i+10)) for i in range(6)],
+    # })
