c8b1067ad4270ec99947dfaf4f11652260ceb475
Asile Ayuba <asileayuba@gmail.com>
2025-02-05 21:34:19 +0100

    Day 36: Create Model

--- drf-project/students/models.py
+++ drf-project/students/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Student(models.Model):
     student_id = models.CharField(max_length=10)
     name = models.CharField(max_length=50)
-    branch = models.CharField(max_length=20)
+    branch = models.CharField(max_length=30)
     
     def __str__(self):
         return self.name
