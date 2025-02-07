d96c959b5e9b3f926a99f0f24bbc8822b7225a59
Asile Ayuba <asileayuba@gmail.com>
2025-02-07 20:30:44 +0100

    Day 38:Function-Based View and GET Method

--- drf-project/api/views.py
+++ drf-project/api/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 # from django.http import JsonResponse
 from students.models import Student
-from .serializers import Student
+from .serializers import StudentSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
