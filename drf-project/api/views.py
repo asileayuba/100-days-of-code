43a3a481f9b5a31fb53036441d60bc40db149af3
Asile Ayuba <asileayuba@gmail.com>
2025-02-08 18:42:20 +0100

    Day 39: Storing Data Using Serializer

--- drf-project/api/views.py
+++ drf-project/api/views.py
@@ -6,10 +6,17 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def studentsViews(request):
     if request.method == 'GET':
         # Get all the data from the Student table
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
