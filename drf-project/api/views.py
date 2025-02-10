697b706d59ff83d930060e88401fa3eb71dceb6e
Asile Ayuba <asileayuba@gmail.com>
2025-02-10 10:13:53 +0100

    Day 41: Updating An Object Using PUT Request

--- drf-project/api/views.py
+++ drf-project/api/views.py
@@ -50,3 +50,5 @@ def studentDetailView(request, pk):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
