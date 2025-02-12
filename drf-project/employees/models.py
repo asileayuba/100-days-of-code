f5d309d52ed66065382c53e3b6b8736ac375f344
Asile Ayuba <asileayuba@gmail.com>
2025-02-12 14:30:44 +0100

    Day 43: Class-Based Views

--- drf-project/employees/models.py
+++ drf-project/employees/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    emp_id = models.CharField(max_length=20)
+    emp_name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=50)
