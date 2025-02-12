b54e9b1c6bdd69afd138ee0d829493d9df961581
Asile Ayuba <asileayuba@gmail.com>
2025-02-12 14:37:56 +0100

    Day 43: Class-Based Views

--- drf-project/employees/models.py
+++ drf-project/employees/models.py
@@ -5,3 +5,6 @@ class Employee(models.Model):
     emp_id = models.CharField(max_length=20)
     emp_name = models.CharField(max_length=50)
     designation = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.emp_name
