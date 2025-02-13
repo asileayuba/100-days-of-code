c6fd6547c23367ca02574a0e540fed3b623d0a6b
Asile Ayuba <asileayuba@gmail.com>
2025-02-13 11:38:08 +0100

    Day 43: Class Based Creation of Employee

--- drf-project/api/views.py
+++ drf-project/api/views.py
@@ -77,5 +77,5 @@ class Employees(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
