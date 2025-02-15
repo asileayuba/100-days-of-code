8a6574883502be82fc617dc9ce738a43638c5e8f
Asile Ayuba <asileayuba@gmail.com>
2025-02-15 17:22:04 +0100

    Day 46: Updating and Deleting an Employee

--- drf-project/api/views.py
+++ drf-project/api/views.py
@@ -103,3 +103,7 @@ class EmployeeDetail(APIView):
     def put(self, request, pk):
         employee = self.get_object(pk)
         serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
