5a34b37624e65045c9b00786fc9c5928fdb98832
Asile Ayuba <asileayuba@gmail.com>
2025-02-23 13:15:37 +0100

    Day 54: Nested Serializers for Blog and Comment Model

--- drf-project/blogs/models.py
+++ drf-project/blogs/models.py
@@ -6,3 +6,7 @@ class Blog(models.Model):
     
     def __str__(self):
         return self.blog_title
+    
+    
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
