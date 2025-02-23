83caec2e806040a650333cf6aa1c33048b34a622
Asile Ayuba <asileayuba@gmail.com>
2025-02-23 13:14:29 +0100

    Day 54: Nested Serializers for Blog and Comment Model

---
+++ drf-project/blogs/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=100)
+    blog_body = models.TextField()
+    
+    def __str__(self):
+        return self.blog_title
