c00cbebecd2b9011f2376a5e74434c43069ea918
Asile Ayuba <asileayuba@gmail.com>
2025-02-26 18:09:51 +0100

    Day 57: Custom Pagination

--- drf-project/api/paginations.py
+++ drf-project/api/paginations.py
@@ -6,3 +6,9 @@ class CustomPagination(PageNumberPagination):
     page_query_param = 'page-num'
     max_page_size =1
     
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            
+        })
