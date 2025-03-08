39c758d9183f62259af4c9729df3110ad42f2616
Asile Ayuba <asileayuba@gmail.com>
2025-03-08 02:17:58 +0100

    Day 67: Build Product Model

--- Ayubuy/store/admin.py
+++ Ayubuy/store/admin.py
@@ -3,7 +3,7 @@ from . models import Product
 
 # Register your models here.
 
-class ProductAdmin(models.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
 
