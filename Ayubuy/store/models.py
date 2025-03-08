ed8988baf8fe5df321a52d0315baf191d90989ee
Asile Ayuba <asileayuba@gmail.com>
2025-03-08 01:58:05 +0100

    Day 67: Build Product Model

--- Ayubuy/store/models.py
+++ Ayubuy/store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from category.models import Category
 
 # Create your models here.
 
@@ -11,4 +12,8 @@ class Product(models.Model):
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.product_name
