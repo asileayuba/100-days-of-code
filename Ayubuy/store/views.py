ac945b51683dbd0c546df977aacad6c30cfec3f7
Asile Ayuba <asileayuba@gmail.com>
2025-03-09 19:12:46 +0100

    Day 68: Store Template Development

--- Ayubuy/store/views.py
+++ Ayubuy/store/views.py
@@ -5,9 +5,11 @@ from .models import Product
 
 
 def store(request):
-    products = Product.objects.filter(is_available=True)  
+    products = Product.objects.filter(is_available=True) 
+    product_count = products.count()
     
     context = {
-        'products': products,  
+        'products': products,
+        'product_count': product_count, 
     }
     return render(request, 'store/store.html', context)
