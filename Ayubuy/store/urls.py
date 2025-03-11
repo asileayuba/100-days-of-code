f3e72394a5bd5c3acca36871ed67c8f5da54d7b4
Asile Ayuba <asileayuba@gmail.com>
2025-03-11 19:26:58 +0100

    Day 70: Get Url For Product

--- Ayubuy/store/urls.py
+++ Ayubuy/store/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'),  # Route for viewing all available products
     path('<slug:category_slug>/', views.store, name='products_by_category'),  # Route for filtering by category
-    path('<slug:category_slug>/<slug:product_slug>', views.product_detail, name='product_detail'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),  # Product detail route
 ]
