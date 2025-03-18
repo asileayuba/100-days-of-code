bb69d3adc5bc356bbc538f52f4afa37445050469
Asile Ayuba <asileayuba@gmail.com>
2025-03-18 10:30:45 +0100

    Day 77: Grouping Cart Item Variations

--- Ayubuy/carts/views.py
+++ Ayubuy/carts/views.py
@@ -63,13 +63,13 @@ def add_cart(request, product_id):
 
     # Retrieve or create a CartItem for the product in the cart
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
         if len(product_variation) > 0:
             cart_item.variations.clear()
             for item in product_variation:
                 cart_item.variations.add(item)
                 
-        cart_item.quantity += 1  # Increase quantity if product already exists
+        # cart_item.quantity += 1  # Increase quantity if product already exists
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
