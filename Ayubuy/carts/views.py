9ce571d7091afff9f369cac5575b786269057e00
Asile Ayuba <asileayuba@gmail.com>
2025-03-18 09:42:25 +0100

    Day 77: Add Variation in Cart Item

--- Ayubuy/carts/views.py
+++ Ayubuy/carts/views.py
@@ -64,6 +64,10 @@ def add_cart(request, product_id):
     # Retrieve or create a CartItem for the product in the cart
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        if len(product_variation) > 0:
+            for item in product_variation:
+                cart_item.variations.add(item)
+                
         cart_item.quantity += 1  # Increase quantity if product already exists
         cart_item.save()
     except CartItem.DoesNotExist:
@@ -72,7 +76,10 @@ def add_cart(request, product_id):
             quantity=1,  # Default quantity is 1
             cart=cart,
         )
-
+        if len(product_variation) > 0:
+            for item in product_variation:
+                cart_item.variations.add(item)
+        cart_item.save()
     return redirect('cart')  # Redirect to the cart page after adding the product
 
 
