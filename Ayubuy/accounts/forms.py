95bd35445f92c4894aeb59b24a980e0e05100905
Asile Ayuba <asileayuba@gmail.com>
2025-03-19 16:28:43 +0100

    Day 78: Form Overriding and Customization

--- Ayubuy/accounts/forms.py
+++ Ayubuy/accounts/forms.py
@@ -3,6 +3,21 @@ from .models import Account
 
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+        'class': 'form-control',
+    }))
+    
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+        
+        
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
