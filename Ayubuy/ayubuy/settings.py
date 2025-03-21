92a9c24d59730035878146b4e83a442cbe7637e7
Asile Ayuba <asileayuba@gmail.com>
2025-03-21 10:09:21 +0100

    Day 80: Account Activation Process

--- Ayubuy/ayubuy/settings.py
+++ Ayubuy/ayubuy/settings.py
@@ -162,3 +162,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+# SMTP Configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'asileayuba@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
