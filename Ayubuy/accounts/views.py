2fbc24a601e3e85eb09da3859245c7d6e19e0995
Asile Ayuba <asileayuba@gmail.com>
2025-03-22 13:46:21 +0100

    Day 81: Forgot Password Functionality, and Reset Password Validatio

--- Ayubuy/accounts/views.py
+++ Ayubuy/accounts/views.py
@@ -151,3 +151,19 @@ def forgotPassword(request):
             messages.error(request, 'Account does not exist!')
             return redirect('forgotPassword')
     return render(request, 'accounts/forgotPassword.html')
+
+
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset your password')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'This link has expired!')
+        return redirect('login')
