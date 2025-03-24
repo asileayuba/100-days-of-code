8dcb12aef45c074fb7bdb1ad2c5250f309243e07
Asile Ayuba <asileayuba@gmail.com>
2025-03-24 21:49:52 +0100

    Day 83: Assign the User to Cart Item

--- Ayubuy/carts/views.py
+++ Ayubuy/carts/views.py
@@ -204,7 +204,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
 
     return render(request, 'store/cart.html', context)  # Render the cart page
 
-@login_required
+@login_required(login_url='login')
 def checkout(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))  # Get the cart
