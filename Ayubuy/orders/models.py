7318fde05eb14dea5279981865941c59ae307c58
Asile Ayuba <asileayuba@gmail.com>
2025-03-26 16:46:13 +0100

    Day 84: Making Order, OrderProduct, and Payment Models

--- Ayubuy/orders/models.py
+++ Ayubuy/orders/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    payment_id = models.CharField(max_length=100)
+    Payment_method = models.CharField(max_length=100)
+    amount_paid = models.CharField(max_length=100)  # this is the total amount paid
+    status =models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.payment_id
+    
+class Order(models.Model):
+    STATUS = {
+        ('New', 'New'),
+        ('Accepted', 'Accepted'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    }
+    
+    user = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
+    Payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
+    order_number = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+    address_line_1 = models.CharField(max_length=50)
+    address_line_2 = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50)
