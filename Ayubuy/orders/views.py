41dd79647efe47cfa49b4ead735f146ca70add29
Asile Ayuba <asileayuba@gmail.com>
2025-03-26 17:30:21 +0100

    Day 84: Place Order and Generate Order Number

--- Ayubuy/orders/views.py
+++ Ayubuy/orders/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from carts.models import CartItem
 
 
 
 def place_order(request):
-    return HttpResponse('Place Order')
+    current_user = request.user
+    
+    
+    # If the cart count is less than or equal to zero, then redirect back to shop
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
