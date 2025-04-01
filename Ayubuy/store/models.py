b93a3832586a90c5d279868ebc679168f07b2359
Asile Ayuba <asileayuba@gmail.com>
2025-04-01 16:09:06 +0100

    Day 91: Review and Rating Model

--- Ayubuy/store/models.py
+++ Ayubuy/store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from category.models import Category
 from django.urls import reverse
 from decimal import Decimal
+from accounts.models import Account
 
 # Model definition for products
 class Product(models.Model):
@@ -69,3 +70,18 @@ class Variation(models.Model):
         ordering = ['product', 'variation_category']
         verbose_name = "Variation"
         verbose_name_plural = "Variations"
+        
+        
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=10, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.subject
