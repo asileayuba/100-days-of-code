cbcba30c3e3a2e01f8261d16798967652a6498f2
Asile Ayuba <asileayuba@gmail.com>
2025-04-03 20:27:05 +0100

    Day 93: User Profile Model

--- Ayubuy/accounts/admin.py
+++ Ayubuy/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from .models import Account, UserProfile
 
 # Custom admin panel configuration for the Account model
 class AccountAdmin(UserAdmin):
@@ -34,6 +34,14 @@ class AccountAdmin(UserAdmin):
 
     # Keeps the default field arrangement (does not modify fieldsets)
     fieldsets = ()  
+    
+    
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="border-radius: 50%;">'.format(object.profile_picture.url))
+    thumbnail.short_description = 'Profile Picture'
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
 
 # Register the Account model with the custom admin panel configuration
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Account, AccountAdmin)
