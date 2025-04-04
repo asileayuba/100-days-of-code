8332ef205ee6577c38454eeafdb4ab8679ac18d3
Asile Ayuba <asileayuba@gmail.com>
2025-04-04 11:58:44 +0100

    Day 94: Edit Profile Functionality

--- Ayubuy/accounts/forms.py
+++ Ayubuy/accounts/forms.py
@@ -58,9 +58,19 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ('first_name', 'last_name', 'phone_number')
+    
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
         
         
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('address_line_1', 'address_line_2', 'city', 'state', 'country', 'profile_picture')   
+        
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
